f447699a328db4aad95fc94011f756ff52b3579a
Joachim Flottorp <joachim@flottorp.org>
2026-09-09 12:33:54 +0200

    multibyte sensitiv string

--- public/_bootstrap.php
+++ public/_bootstrap.php
@@ -3,8 +3,8 @@
 session_start();
 
 $scriptDir = str_replace("\\", "/", dirname($_SERVER["SCRIPT_NAME"]));
-if (substr($scriptDir, -7) === "/public") {
-    $scriptDir = substr($scriptDir, 0, -7);
+if (mb_substr($scriptDir, -7) === "/public") {
+    $scriptDir = mb_substr($scriptDir, 0, -7);
 }
 if ($scriptDir === "/") {
     $scriptDir = "";
