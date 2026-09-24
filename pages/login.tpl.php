f3f74335578f50980145d52be7fb1a430c0ce637
Benhofi <Benjaminfi@uia.no>
2026-09-24 15:25:57 +0200

    fix1

--- pages/login.tpl.php
+++ pages/login.tpl.php
@@ -11,12 +11,36 @@
             </ul>
         <?php endif; ?>
 
-        
+        <form method="post" action="" class="form">
+
+            <div class="form-felt">
+                <label for="epost">E-post</label>
+                <input
+                    type="email"
+                    id="epost"
+                    name="epost"
+                    autocomplete="email"
+                    value="<?php echo htmlspecialchars($epost ?? ''); ?>"
+                    required
+                >
+            </div>
+
+            <div class="form-felt">
+                <label for="passord">Passord</label>
+                <input
+                    type="password"
+                    id="passord"
+                    name="passord"
+                    autocomplete="current-password"
+                    required
+                >
+            </div>
+
+            <button type="submit" class="button button-primary button-lg">
+                Logg inn
+            </button>
+
+        </form>
 
         <p class="auth-lenke">
-            Ikke registrert?
-            <?php echo url("/registrer.php"); ?>Registrer deg</a>
-        </p>
-
-    </section>
-</div>
+        
