bc0561101b0dbbb69e45cb17e898892c3cf940a2
Benhofi <Benjaminfi@uia.no>
2026-09-24 13:40:44 +0200

    Update _bootstrap.php

--- public/_bootstrap.php
+++ public/_bootstrap.php
@@ -4,10 +4,6 @@ session_start();
 
 $scriptDir = str_replace("\\", "/", dirname($_SERVER["SCRIPT_NAME"]));
 
-if (mb_substr($scriptDir, -7) === "/public") {
-    $scriptDir = mb_substr($scriptDir, 0, -7);
-}
-
 if ($scriptDir === "/") {
     $scriptDir = "";
 }
@@ -17,13 +13,6 @@ define("BASE_URL", $scriptDir);
 function url(string $path): string {
     return BASE_URL . $path;
 }
-
-
-/*
-|--------------------------------------------------------------------------
-| Database
-|--------------------------------------------------------------------------
-*/
 
 $database_host = "127.0.0.1";
 $database_navn = "studenthub";
